d372b4302f42e21dd349459ac835c95e084b5c3b
Baptiste <baptiste.deligny@protonmail.ch>
2017-11-05 22:52:25 +0100

    First complete version, not working

--- secretDeShamir.c
+++ secretDeShamir.c
@@ -2,7 +2,7 @@
 #include "secretDeShamir.h"
 
 
-int constrPolynom(mpz_t* factors, int degree, const mpz_t secret)
+int constrPolynom(s_poly* poly, const mpz_t secret)
 {
 	int i;
 	char* seed;
@@ -15,11 +15,15 @@ int constrPolynom(mpz_t* factors, int degree, const mpz_t secret)
 	randomFic = fopen("/dev/urandom", "r");
 
 	//We put the secret in the first spot of the array (the secret being the constant of the resulting polynomial)
-	mpz_init(factors[0]);
-	mpz_set(factors[0], secret);
+	mpz_init(poly->factors[0]);
+	mpz_set(poly->factors[0], secret);
+
+
+	genPrimeGt(poly->mod, secret);
+
 
 	//for each remaining elements of the array
-	for(i = 1; i < degree; i++)
+	for(i = 1; i < poly->degree; i++)
 	{
 		//we read random data from /dev/urandom
 		fread(&seed, 1, sizeof(unsigned long int), randomFic);
@@ -29,46 +33,122 @@ int constrPolynom(mpz_t* factors, int degree, const mpz_t secret)
 		gmp_randseed(state, seedMpz);
 		
 		//we fill our array with the random value
-		mpz_init(factors[i]);
-		mpz_urandomb(factors[i], state, 32);
+		mpz_init(poly->factors[i]);
+		mpz_urandomb(poly->factors[i], state, 128);
+
 	}
-	
+		
 	//clearing memory
 	close(randomFic);
 	mpz_clear(seedMpz);
 	return(0);
 }
 
-int resPolynom(mpz_t* results, const mpz_t* polynom, int degree, int nbPart)
+int resPolynom(mpz_t* results, const s_poly* poly, int nbAllPart)
 {
 	int i;
 	int j;
 
 	mpz_t buffer;
 	mpz_init(buffer);
-	
+
+
 	//for each part to create (ie n)	
-	for(i=0; i<nbPart; i++)
+	for(i=0; i<nbAllPart; i++)
 	{
 		
 		mpz_init(results[i]);
 		mpz_set_ui(results[i], 0);
 		
 		//we resolve the polynom of degree (degree-1)(ie k)
-		for(j=0; j < degree; j++)
+		for(j=0; j < poly->degree; j++)
 		{
 			mpz_set_ui(buffer, 0);
 			
 			//we multiply the factor with x^j (x being just 1 to nbpart)
-			mpz_mul_ui(buffer, polynom[j], (unsigned long int)(pow((double)(i+1), (double)j)));
+			mpz_mul_ui(buffer, poly->factors[j], (unsigned long int)(pow((double)(i+1), (double)j)));
 
 			mpz_add(results[i], results[i], buffer);
+
 		}
 
+		mpz_mod(results[i], results[i], poly->mod);
 	}	
 	
 	//clearing memory
 	mpz_clear(buffer);
 	
+	return(0);
+}
+
+int interLagrange(mpz_t secret, const mpz_t* parts, int nbParts)
+{
+	//iterators
+	int i, j;
+	//buffers
+	signed long int buff2;
+	mpz_t buff1;
+
+	mpz_init(buff1);
+
+	mpz_set_ui(buff1, (unsigned long int)0);
+	
+	//calculates the reduced lagrange's polynom (to only get the constant part, i.e. the secret)
+	for(i=0; i < nbParts; i++)
+	{
+		mpz_set_ui(buff1, 1);
+		for(j=0; j<nbParts; j++)
+		{
+			if(i != j)
+			{
+				buff2 = (j+1) / (j-i);
+
+				mpz_mul_si(buff1, buff1, buff2);
+			}
+		}
+		mpz_mul(buff1, buff1, parts[i]);
+
+		mpz_add(secret, secret, buff1);
+	}
+
+
+	mpz_clear(buff1);
+	return(0);
+}
+
+int genPrimeGt(mpz_t prime, const mpz_t lowerLim)
+{
+	
+	FILE* randFic = NULL;
+	char* seed = NULL;
+	mpz_t seedMpz;
+	gmp_randstate_t state;
+
+	gmp_randinit_default(state);
+	mpz_init(seedMpz);
+	mpz_init(prime);
+	
+	//open the /dev/urandom file
+	randFic = fopen("/dev/urandom", "r");
+
+	do
+	{
+		//reads 32bits from /dev/urandom
+		fread(&seed, 1, sizeof(unsigned long int), randFic);
+		
+		mpz_set_ui(seedMpz, (unsigned long int)seed);
+
+		gmp_randseed(state, seedMpz);
+
+		mpz_urandomb(prime, state, 128);
+
+		//add the lowerlimit to be sure to be above
+		mpz_add(prime, prime, lowerLim);		
+	//loop while the rabinMiller test fails
+	}while(isPrime(prime) == 0);
+	
+	close(randFic);
+	mpz_clear(seedMpz);
+
 	return(0);
 }
--- secretDeShamir.h
+++ secretDeShamir.h
@@ -4,8 +4,44 @@
 #include <stdio.h>
 #include <math.h>
 #include <gmp.h>
+#include "RabinMiller/rabinMiller.h"
 
-int constrPolynom(mpz_t* factors, int degree, const mpz_t secret);
+typedef struct polynom s_poly;
 
-int resPolynom(mpz_t* results, const mpz_t* polynom, int degree, int nbPart);
+struct polynom
+{
+	mpz_t* factors;
+	mpz_t mod;
+	int degree;
+};
+
+/*Constructs a polynom with the secret as the constant and random number as the others factors*/
+//OUT:
+//	s_poly poly	: resulting polynom
+//IN:
+//	secret		: secret to share
+int constrPolynom(s_poly* poly, const mpz_t secret);
+
+/*Resolve the polynom for nbParts point betwen ]0; nbparts]*/
+//OUT:
+//	results		: resulting points
+//IN:
+//	poly		: polynom
+//	nbpart		: number of parts to share
+int resPolynom(mpz_t* results, const s_poly* poly, int nbPart);
+
+/*Realises a Lagrange interpolation, returning the constant from the resulting polynom*/
+//OUT:
+//	secret		: constant of the polynom, the shared secret
+//IN:
+//	parts		: points from the polynom, parts of the secret
+//	nbParts		: number of parts
+int interLagrange(mpz_t secret, const mpz_t* parts, int nbPart);
+
+/*generates a random prime number greater than lowerLim*/
+//OUT:
+//	prime		: resultaing prime number
+//IN:
+//	lowerLimit	: lower limit to the prime number
+int genPrimeGt(mpz_t prime, const mpz_t lowerLim);
 #endif
